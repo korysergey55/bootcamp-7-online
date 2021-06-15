2d0fb50aec0eb82f7701330d0d2f3bee76b086ef
yaroslav-kulpan <51779177+yaroslav-kulpan@users.noreply.github.com>
2021-06-15 13:21:50 +0300

    +++

--- lesson-6-rest-api/src/shared/components/Modal/Modal.jsx
+++ lesson-6-rest-api/src/shared/components/Modal/Modal.jsx
@@ -1,10 +1,5 @@
 import React, { Component, createRef } from "react";
 import { createPortal } from "react-dom";
-import {
-  clearAllBodyScrollLocks,
-  disableBodyScroll,
-  enableBodyScroll,
-} from "body-scroll-lock";
 import PropTypes from "prop-types";
 
 const modalRoot = document.getElementById("modal-root");
@@ -19,15 +14,11 @@ class Modal extends Component {
 
   componentDidUpdate(prevProps, prevState) {
     if (this.ref.current) {
-      console.log(this.ref.current, 'ref', 'work')
-      if (this.props.open) {
-        window.addEventListener("keydown", this.handleKeyDown);
-        disableBodyScroll(this.ref.current);
-      } else {
-        console.log('work')
-        window.removeEventListener("keydown", this.handleKeyDown);
-        enableBodyScroll(this.ref.current);
-      }
+      window.addEventListener("keydown", this.handleKeyDown);
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+      window.removeEventListener("keydown", this.handleKeyDown);
     }
   }
 
@@ -48,7 +39,7 @@ class Modal extends Component {
 
   componentWillUnmount() {
     window.removeEventListener("keydown", this.handleKeyDown);
-    clearAllBodyScrollLocks();
+    document.body.style.overflow = "auto";
   }
 
   render() {
@@ -60,7 +51,10 @@ class Modal extends Component {
           role="dialog"
           aria-modal="true"
         >
-          <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+          <div
+            ref={this.ref}
+            className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0"
+          >
             <div
               className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
               aria-hidden="true"
@@ -73,17 +67,15 @@ class Modal extends Component {
             >
               &#8203;
             </span>
-            <div  ref={this.ref} className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+            <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
               <button
-                  onClick={this.props.onClose}
-                  type="button"
-                  className="bg-red-500 px-2 py-2 text-white"
+                onClick={this.props.onClose}
+                type="button"
+                className="bg-red-500 px-2 py-2 text-white"
               >
                 Крестик
               </button>
-              <div
-                className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4"
-              >
+              <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
                 <div className="sm:flex flex-column sm:items-start">
                   {this.props.children}
                 </div>
