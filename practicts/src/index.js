cd302826a2a59dfd35f502102449d3322b7547bb
yaroslav-kulpan <51779177+yaroslav-kulpan@users.noreply.github.com>
2021-06-25 10:36:55 +0300

    Add redux-persist and ignoredActions for redux-persist

--- practicts/src/index.js
+++ practicts/src/index.js
@@ -4,12 +4,15 @@ import { Provider } from "react-redux";
 
 import App from "./components/App";
 import "./styles/global.css";
-import store from "./redux/configure-store";
+import { store, persistor } from "./redux/configure-store";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
--- practicts/src/redux/configure-store.js
+++ practicts/src/redux/configure-store.js
@@ -1,13 +1,38 @@
-import { configureStore} from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import {
+  persistReducer,
+  persistStore,
+  PERSIST,
+  REGISTER,
+  PAUSE,
+  PURGE,
+  FLUSH,
+  REHYDRATE,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
 import shoppingCartReducer from "./shopping-cart/shopping-cart.reducer";
 
+const persistedCartReducer = persistReducer(
+  { key: "shopping-cart", storage },
+  shoppingCartReducer
+);
 
 const store = configureStore({
+  middleware: [
+    ...getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [PURGE, PERSIST, REGISTER, REHYDRATE, PAUSE, FLUSH],
+      },
+    }),
+  ],
   reducer: {
-    cart: shoppingCartReducer,
+    cart: persistedCartReducer,
     // wishlist: ,
     // callback: ,
   },
 });
 
-export default store;
+const persistor = persistStore(store);
+
+export { store, persistor };
