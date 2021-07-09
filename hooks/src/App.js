256100b582a4daaa10f39d4886a81d6ea56984b9
yaroslav-kulpan <51779177+yaroslav-kulpan@users.noreply.github.com>
2021-07-09 12:46:14 +0300

    +++

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,4 +1,12 @@
-import { useContext, useEffect, useReducer, useRef } from "react";
+import {
+  useCallback,
+  useContext,
+  useEffect,
+  useMemo,
+  useReducer,
+  useRef,
+  useState,
+} from "react";
 
 import "./App.css";
 import { ThemeContext } from "./context/theme.context";
@@ -57,21 +65,81 @@ function useGeoPosition() {
   return state;
 }
 
+function useAsync(callback, ...args) {
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(null);
+
+  const memoCallback = useCallback(
+    (...args) => {
+      setLoading(true);
+      // console.log(args)
+      callback(...args)
+        .then((data) => setData(data))
+        .catch((err) => setError(err))
+        .finally(() => setLoading(false));
+    },
+    [callback]
+  );
+
+  const memoArgs = useMemo(() => args.join(","), [args]);
+
+  useEffect(() => {
+    memoCallback(memoArgs);
+  }, [memoArgs, memoCallback]);
+
+  return {
+    loading,
+    data,
+    error,
+  };
+}
+
+const fetchData = (id) => {
+  return fetch(`https://jsonplaceholder.typicode.com/posts/${id}`).then((res) =>
+    res.json()
+  );
+};
+
+const fetchTodos = (id) => {
+  return fetch(`https://jsonplaceholder.typicode.com/todos/${id}`).then((res) =>
+    res.json()
+  );
+};
+
 function App() {
+  const [post, setPost] = useState(1);
+  const [todo, setTodo] = useState(1);
+  const refInput = useRef();
   const { positions, loading } = useGeoPosition();
   const { theme, setTheme } = useContext(ThemeContext);
+  const { data } = useAsync(fetchData, post);
+  const { data: todos } = useAsync(fetchTodos, todo);
+
+  useEffect(() => {
+    if (todo > 3) {
+      refInput.current.focus();
+    }
+  }, [todo]);
 
   return (
     <div>
+      <h3>Input</h3>
+      <input type="text" ref={refInput} />
       <h3>Hello</h3>
-
+      <button onClick={() => setPost((prevState) => prevState + 1)}>
+        New Post
+      </button>
+      <h3>Post:</h3>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+      <h3>Todo:</h3>
+      <pre>{JSON.stringify(todos, null, 2)}</pre>
+      <button onClick={() => setTodo((prevState) => prevState + 1)}>
+        New Todos
+      </button>
       <p>{theme === "light" ? <p>Light</p> : <p>Dark</p>}</p>
-      <button onClick={() => setTheme("dark")}>
-        Dark
-      </button>
-      <button onClick={() => setTheme("light")}>
-        Light
-      </button>
+      <button onClick={() => setTheme("dark")}>Dark</button>
+      <button onClick={() => setTheme("light")}>Light</button>
       {loading ? (
         <p>Loading....</p>
       ) : (
--- hooks/src/context/theme.context.js
+++ hooks/src/context/theme.context.js
@@ -1,19 +1,18 @@
-import {createContext, useState} from 'react';
+import { createContext, useState } from "react";
 
 const InitialThemeContext = {
-  theme: 'light',
-  setTheme: () => null
+  theme: "light",
+  setTheme: () => null,
 };
 
 export const ThemeContext = createContext(InitialThemeContext);
 
-
 export const ThemeContextProvider = ({ children }) => {
-  const [theme, setTheme] = useState('light');
+  const [theme, setTheme] = useState("light");
 
-  return(
-      <ThemeContext.Provider value={{ theme, setTheme }}>
-        {children}
-      </ThemeContext.Provider>
-  )
-}
+  return (
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
